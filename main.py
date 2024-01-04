f8d18bce27810b5e4528057e983415c4a7418f72
mimingfikriac <149293639+mimingfikriac@users.noreply.github.com>
2024-01-04 19:24:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -66,9 +66,9 @@ if __name__ == '__main__':
     main_time = LogTime(words="Main Function")
     # dataset_name = 'ml-100k'
     dataset_name = 'ml-1m'
-    # model_type = 'UserCF'
+    model_type = 'UserCF'
     # model_type = 'UserCF-IIF'
-    model_type = 'ItemCF'
+    # model_type = 'ItemCF'
     # model_type = 'Random'
     # model_type = 'MostPopular'
     # model_type = 'ItemCF-IUF'
